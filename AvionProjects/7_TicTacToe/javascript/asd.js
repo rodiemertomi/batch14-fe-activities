c75ae4283c9157f1e18cb4fbfc262494c9bd972f
Rodiemer Tominez <rodiemertominez@gmail.com>
2021-11-14 10:55:29 +0800

    Finished tictactoe

--- AvionProjects/7_TicTacToe/javascript/asd.js
+++ AvionProjects/7_TicTacToe/javascript/asd.js
@@ -1,213 +1,156 @@
-//
-// Variables
-//
-
-var baseState = function () {
-	return [null, null, null, null, null, null, null, null, null];
-};
-var historyState = [];
-var currentState, turn;
-
-
-//
-// Methods
-//
-
-/**
- * Check if there's a winner
- */
-var isWinner = function () {
-
-	// Possible winning combinations
-	var wins = [
-		[0,1,2],
-		[3,4,5],
-		[6,7,8],
-		[0,3,6],
-		[1,4,7],
-		[2,5,8],
-		[0,4,8],
-		[2,4,6]
-	];
-
-	// Check if there's a winning combo
-	var isWinner = wins.filter(function (win) {
-		return (currentState[win[0]] && currentState[win[0]] === currentState[win[1]] && currentState[win[0]] === currentState[win[2]]);
-	});
-
-	// Return the winner, or false if there isn't one
-	return (isWinner.length > 0 ? currentState[isWinner[0][0]] : false);
-
-};
-
-/**
- * Check if the square is first in the row
- * @param  {Integer}  id The ID for the square
- * @return {Boolean}     Returns true if first in the row
- */
-var isFirstInRow = function (id) {
-	return (id + 1) % 3 === 1;
-};
-
-/**
- * Check if the square is last in the row
- * @param  {Integer}  id The ID for the square
- * @return {Boolean}     Returns true if last in the row
- */
-var isLastInRow = function (id) {
-	return (id + 1) % 3 === 0;
-};
-
-/**
- * Build each square of the game board
- * @param  {Array}   state  The board state
- * @param  {Boolean} winner If true, someone won the game
- * @return {String}         The markup
- */
-var buildSquares = function (state, winner) {
-
-	// Setup rows
-	var rows = '';
-
-	// Loop through each square in the state
-	state.forEach(function (square, id) {
-
-		// Variables
-		var value = square ? square : '';
-		var selected = square ? ' aria-pressed="true"' : '';
-		var disabled = square || winner ? ' disabled' : '';
-
-		// Check if it's a new row
-		if (isFirstInRow(id)) {
-			rows += '<tr>';
-		}
-		rows += '<td><button class="game-square" data-id="' + id + '"' + selected + disabled + '>' + value + '</button></td>';
-
-		// Check if it's the last column in a row
-		if (isLastInRow(id)) {
-			rows += '</tr>';
-		}
-
-	});
-
-	return rows;
-
-};
-
-/**
- * Build the history state buttons markup
- * @return {String} The markup
- */
-var buildHistory = function () {
-
-	// Setup history markup
-	var history = '';
-
-	// If there's a history state, loop through each state and build a button
-	if (historyState.length > 0) {
-		history += '<h2>Game History</h2><ol>';
-		historyState.forEach(function (move, index) {
-			history += '<li><button data-history="' + move.toString() + '">Go to move # ' + (index + 1) + '</button></li>';
-		});
-		history += '</ol>';
-	}
-
-	return history;
-
-};
-
-/**
- * Build the game board
- * @param  {Array} state The state to build from
- * @return {String}      The markup based on the state
- */
-var buildBoard = function (state) {
-
-	// Check if there's a winner
-	var winner = isWinner();
-
-	// Setup the board
-	var rows = winner ? '<p><strong>' + winner + ' is the winner!</string></p>' : '';
-	rows += '<table><tbody>';
-
-	// Create each square
-	rows += buildSquares(state, winner);
-	rows += '</tbody></table><p><button id="play-again">Play Again</button></p>';
-
-	// Create game history
-	rows += buildHistory();
-
-	return rows;
-};
-
-/**
- * Update the board based on a state
- * @param  {Array} state The state to update from (optional, defaults to currentState)
- */
-var updateBoard = function (state) {
-	var board = document.querySelector('#game-board')
-	if (!board) return;
-	board.innerHTML = buildBoard(state || currentState);
-};
-
-/**
- * Render the board again based on the current user's turn
- * @param  {Node} square The square that was selected
- */
-var renderTurn = function (square) {
-
-	// Get selected value
-	var selected = square.getAttribute('data-id');
-	if (!selected) return;
-
-	// Update state
-	currentState[selected] = turn;
-
-	// Add a historical state
-	historyState.push(currentState.slice());
-
-	// Render with new state
-	updateBoard();
-
-	// Update turn
-	turn = turn === 'X' ? 'O' : 'X';
-
-};
-
-/**
- * Reset the board to it's base state
- */
-var resetBoard = function () {
-	currentState = baseState();
-	historyState = [];
-	turn = 'X';
-	updateBoard();
-};
+const winningCombinations =[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+const darkSide = 'dark-side'
+const lightSide = 'light-side'
+const cells = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageText = document.querySelector('[data-winning-message-text]')
+const winningMessage = document.getElementById('winningMessage')
+const restart = document.getElementById('restartButton')
+const side = document.getElementById('sides')
+const darkSideButton = document.getElementById('darkSideButton')
+const lightSideButton = document.getElementById('lightSideButton')
+const previousButton = document.getElementById('previousButton')
+const nextButton = document.getElementById('nextButton')
+let boardState = new Array(9)
+let history = []
+let turn
+let pointer = history.length
 
 
-//
-// Inits & Event Listeners
-//
+startGame()
+restart.addEventListener('click', startGame)
+darkSideButton.addEventListener('click', theDarkSide)
+lightSideButton.addEventListener('click', theLightSide)
+previousButton.addEventListener('click', previous)
+nextButton.addEventListener('click', next)
 
-// Setup the board
-resetBoard();
+function startGame(){
+    side.classList.add('show')
+    cells.forEach(cell=>{
+        cell.classList.remove(lightSide)
+        cell.classList.remove(darkSide)
+        cell.classList.remove('disabled')
+        cell.removeEventListener('click', clicked)
+        cell.addEventListener('click', clicked, {once: true})
+    })
+    winningMessage.classList.remove('show')
+}
 
-// Listen for selections
-document.addEventListener('click', function (event) {
+function clicked(e){
+    const cellTarget = e.target
+    const currentClass = turn ? lightSide : darkSide
+    placeMark(cellTarget, currentClass)
+    checkState()
+    if(checkWin(currentClass)){
+        endGame(false)
+    } else if(isDraw()){
+        endGame(true)
+    } else{
+        switchTurns()
+        setBoardHoverClass()
+    }
+}
 
-	// If a .game-square was clicked
-	if (event.target.matches('.game-square') && !event.target.hasAttribute('disabled')) {
-		renderTurn(event.target);
-	}
+function endGame(draw){
+    if (draw){
+        winningMessageText.innerText = "Draw!"
+    }else{
+        winningMessageText.innerText = `${turn ? "The Light Side" : "The Dark Side"} WINS!`
+    }
+    winningMessage.classList.add('show')
+    cells.forEach(cell=>{
+        cell.classList.add('disabled')
+        cell.removeEventListener('click', clicked)
+        cell.removeAttribute('hover')
+    })
+    previousButton.classList.add('show')
+    nextButton.classList.add('show')
+}
 
-	// If #play-again was clicked
-	if (event.target.matches('#play-again')) {
-		resetBoard();
-	}
+function isDraw(){
+    return [...cells].every(cell => {
+        return cell.classList.contains(darkSide) || cell.classList.contains(lightSide)
+    })
+}
 
-	// If a historical button was clicked
-	if (event.target.matches('[data-history]')) {
-		updateBoard(event.target.getAttribute('data-history').split(','));
-	}
+function placeMark(cell, currentClass){
+    cell.classList.add(currentClass)
+}
 
-}, false);
+function switchTurns(){
+    turn = !turn
+}
+
+function setBoardHoverClass(){
+    board.classList.remove(darkSide)
+    board.classList.remove(lightSide)
+    if(turn){
+        board.classList.add(lightSide)
+    }else{
+        board.classList.add(darkSide)
+    }
+}
+
+function checkWin(currentClass){
+    return winningCombinations.some(combination => {
+        return combination.every(index => {
+            return cells[index].classList.contains(currentClass)
+        })
+    })
+}
+
+function theDarkSide(){
+    turn = false
+    setBoardHoverClass()
+    side.classList.remove('show')
+}
+
+function theLightSide(){
+    turn = true
+    setBoardHoverClass()
+    side.classList.remove('show')
+}
+
+function checkState(){
+    for(let i = 0; i<boardState.length; i++){
+        if(cells[i].classList.contains(darkSide)){
+            boardState[i] = 'X'
+        }else if(cells[i].classList.contains(lightSide)){
+            boardState[i] = 'O'
+        }else{
+            boardState[i] = ''
+        }
+    }
+    let boardStateCopy = [...boardState]
+    history.push(boardStateCopy)
+    console.log(history)
+}
+
+function previous(){
+    if(pointer === 0){
+        previousButton.setAttribute('disabled')
+    }else if(pointer > 0){
+        previousButton.removeAttribute('disabled')
+        pointer -= 1
+    }
+    
+}
+
+function next(){
+    if(pointer === history.length){
+        nextButton.setAttribute('disabled')
+    }else if(pointer < history.length){
+        nextButton.removeAttribute('disabled')
+        pointer += 1
+    }
+    
+}
--- AvionProjects/7_TicTacToe/javascript/index.js
+++ AvionProjects/7_TicTacToe/javascript/index.js
@@ -8,12 +8,6 @@ const winningCombinations =[
     [0,4,8],
     [2,4,6]
 ]
-let board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-let history = []
 const darkSide = 'dark-side'
 const lightSide = 'light-side'
 const cells = document.querySelectorAll('[data-cell]')
@@ -24,12 +18,20 @@ const restart = document.getElementById('restartButton')
 const side = document.getElementById('sides')
 const darkSideButton = document.getElementById('darkSideButton')
 const lightSideButton = document.getElementById('lightSideButton')
+const previousButton = document.getElementById('previousButton')
+const nextButton = document.getElementById('nextButton')
+let boardState = new Array(9)
+let history = []
 let turn
+let pointer = -1
+let isGameEnd
 
 startGame()
-restart.addEventListener('click', startGame)
+restart.addEventListener('click', restartGame)
 darkSideButton.addEventListener('click', theDarkSide)
 lightSideButton.addEventListener('click', theLightSide)
+previousButton.addEventListener('click', previous)
+nextButton.addEventListener('click', next)
 
 function startGame(){
     side.classList.add('show')
@@ -47,8 +49,8 @@ function clicked(e){
     const cellTarget = e.target
     const currentClass = turn ? lightSide : darkSide
     placeMark(cellTarget, currentClass)
-    history.push(cellTarget)
-    console.log(history)
+    pointer += 1
+    checkState()
     if(checkWin(currentClass)){
         endGame(false)
     } else if(isDraw()){
@@ -62,12 +64,16 @@ function clicked(e){
 function endGame(draw){
     if (draw){
         winningMessageText.innerText = "Draw!"
-        cells.removeEventListener('click', clicked)
     }else{
         winningMessageText.innerText = `${turn ? "The Light Side" : "The Dark Side"} WINS!`
-        cells.removeEventListener('click', clicked)
     }
     winningMessage.classList.add('show')
+    cells.forEach(cell=>{
+        cell.classList.add('disabled')
+        cell.removeEventListener('click', clicked)
+        cell.removeAttribute('hover')
+    })
+    endState()
 }
 
 function isDraw(){
@@ -112,4 +118,84 @@ function theLightSide(){
     turn = true
     setBoardHoverClass()
     side.classList.remove('show')
+}
+
+function checkState(){
+    for(let i = 0; i<boardState.length; i++){
+        if(cells[i].classList.contains(darkSide)){
+            boardState[i] = 'X'
+        }else if(cells[i].classList.contains(lightSide)){
+            boardState[i] = 'O'
+        }else{
+            boardState[i] = ''
+        }
+    }
+    let boardStateCopy = [...boardState]
+    history.push(boardStateCopy)
+}
+
+function restartGame(){
+    history = []
+    pointer = -1
+    boardState = []
+    previousButton.classList.remove('show')
+    nextButton.classList.remove('show')
+    previousButton.classList.remove('disabled')
+    nextButton.classList.remove('disabled')
+    startGame()
+}
+
+function previous(){
+    cells.forEach(cell=>{
+        cell.classList.remove(darkSide)
+        cell.classList.remove(lightSide)
+    })
+    pointer -= 1
+    let nextMove = history[pointer]
+    for(let i = nextMove.length; i>=0; i--){
+        if(nextMove[i] === 'X'){
+            cells[i].classList.add(darkSide)
+        }else if(nextMove[i] === 'O'){
+            cells[i].classList.add(lightSide)
+        }
+    }
+    endState()
+}
+
+function next(){
+    cells.forEach(cell=>{
+        cell.classList.remove(darkSide)
+        cell.classList.remove(lightSide)
+    })
+    pointer +=1
+    let nextMove = history[pointer]
+    for(let i = 0; i<=nextMove.length; i++){
+        if(nextMove[i] === 'X'){
+            cells[i].classList.add(darkSide)
+        }else if(nextMove[i] === 'O'){
+            cells[i].classList.add(lightSide)
+        }
+    }
+    endState()
+}
+
+function endState(){
+    previousButton.classList.add('show')
+    nextButton.classList.add('show')
+    if(pointer === 0){
+        previousButton.disabled = true
+        previousButton.style.cursor = 'not-allowed'
+        previousButton.classList.add('disabled')
+    }else if(pointer === history.length-1){
+        nextButton.disabled = true
+        nextButton.style.cursor = 'not-allowed'
+        nextButton.classList.add('disabled')
+    }else{
+        previousButton.disabled = false
+        previousButton.style.cursor = 'pointer'
+        previousButton.classList.remove('disabled')
+        nextButton.disabled = false
+        nextButton.style.cursor = 'pointer'
+        nextButton.classList.remove('disabled')
+    }
 }
